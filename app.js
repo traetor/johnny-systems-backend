d361545f435e02016c54dad81d29f454016494ee
Johny <jasiek.krol.pl@gmail.com>
2024-07-10 13:50:00 +0200

    test database connect

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors'); // Dodaj import cors
+const { Pool } = require('pg'); // Dodaj import Pool z pg
 
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
@@ -24,7 +25,26 @@ const corsOptions = {
 // Dodaj middleware CORS przed zdefiniowanymi trasami API
 app.use(cors(corsOptions));
 
-// Dodaj trasę dla głównej ścieżki
+// Połączenie z bazą danych PostgreSQL
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // W przypadku używania SSL
+    }
+});
+
+// Endpoint do testowania połączenia z bazą danych
+app.get('/test-db-connection', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        client.release();
+        res.send('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        res.status(500).send('Error connecting to the database');
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Manager App');
 });
