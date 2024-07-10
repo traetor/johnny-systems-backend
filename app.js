d87a72db016216cd5d77b6447254ad3bebbc8559
Johny <jasiek.krol.pl@gmail.com>
2024-07-10 14:40:49 +0200

    vercel route fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors'); // Dodaj import cors
-const { Pool } = require('pg'); // Dodaj import Pool z pg
 
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
@@ -25,26 +24,7 @@ const corsOptions = {
 // Dodaj middleware CORS przed zdefiniowanymi trasami API
 app.use(cors(corsOptions));
 
-// Połączenie z bazą danych PostgreSQL
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // W przypadku używania SSL
-    }
-});
-
-// Endpoint do testowania połączenia z bazą danych
-app.get('/test-db-connection', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        client.release();
-        res.send('Connected to the database');
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-        res.status(500).send('Error connecting to the database');
-    }
-});
-
+// Dodaj trasę dla głównej ścieżki
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Manager App');
 });
