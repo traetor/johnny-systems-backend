8a78d27b62ab5d296058011a70f0fecb010deb8c
Johnny <jasiek.krol.pl@gmail.com>
2024-08-26 11:33:25 +0200

    recaptcha v3 fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
+const axios = require('axios');
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const transporter = nodemailer.createTransport({
     service: "Gmail",
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
@@ -21,33 +22,41 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+// Funkcja weryfikująca reCAPTCHA
+const verifyRecaptcha = async (recaptchaToken) => {
+    const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+    const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptchaToken}`;
+
+    try {
+        const response = await axios.post(verificationUrl);
+        return response.data.success;
+    } catch (err) {
+        console.error('Error verifying reCAPTCHA:', err);
+        return false;
+    }
+};
+
 exports.register = (req, res) => {
     const { username, email, password } = req.body;
-    const { language } = req.body;
 
-    // Sprawdzenie, czy podano email i hasło
     if (!email || !password) {
         return res.status(400).send({ message: `Email and password are required` });
     }
 
-    // Generowanie tokenu aktywacyjnego
     const activationToken = crypto.randomBytes(20).toString('hex');
 
-    // Haszowanie hasła
     bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
             console.error('Error hashing password:', err);
             return res.status(500).send({ message: `Error during registration. Please try again later.` });
         }
 
-        // Tworzenie użytkownika
         User.create({ username, email, password: hash, avatar: null, activation_token: activationToken }, (err, result) => {
             if (err) {
                 console.error('Error creating user:', err);
                 return res.status(500).send({ message: `Error during registration. Please try again later.` });
             }
 
-            // Wysyłanie emaila aktywacyjnego
             const activationLink = `${process.env.FRONTEND_URL}/activate/${activationToken}`;
             const mailOptions = {
                 from: process.env.EMAIL,
@@ -84,35 +93,39 @@ exports.activate = (req, res) => {
     });
 };
 
-exports.login = (req, res) => {
-    const { email, password } = req.body;
+exports.login = async (req, res) => {
+    const { email, password, recaptchaToken } = req.body;
 
-    // Sprawdzenie, czy podano email i hasło
-    if (!email || !password) {
-        return res.status(400).send({ message: `Email and password are required` });
+    if (!email || !password || !recaptchaToken) {
+        return res.status(400).send({ message: 'Email, password, and reCAPTCHA token are required' });
+    }
+
+    const isRecaptchaValid = await verifyRecaptcha(recaptchaToken);
+    if (!isRecaptchaValid) {
+        return res.status(400).send({ message: 'Invalid reCAPTCHA. Please try again.' });
     }
 
     User.findByEmail(email, (err, users) => {
         if (err) {
             console.error('Error finding user by email:', err);
-            return res.status(500).send({ message: `Error finding user. Please try again later.` });
+            return res.status(500).send({ message: 'Error finding user. Please try again later.' });
         }
         if (users.length === 0) {
-            return res.status(404).send({ message: `User not found` });
+            return res.status(404).send({ message: 'User not found' });
         }
 
         const user = users[0];
         if (!user.is_active) {
-            return res.status(403).send({ message: `Account is not activated. Please check your email for activation link.` });
+            return res.status(403).send({ message: 'Account is not activated. Please check your email for activation link.' });
         }
 
         bcrypt.compare(password, user.password, (err, match) => {
             if (err) {
                 console.error('Error comparing passwords:', err);
-                return res.status(500).send({ message: `Error comparing passwords. Please try again later.` });
+                return res.status(500).send({ message: 'Error comparing passwords. Please try again later.' });
             }
             if (!match) {
-                return res.status(401).send({ message: `Invalid credentials` });
+                return res.status(401).send({ message: 'Invalid credentials' });
             }
 
             const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
@@ -130,13 +143,11 @@ exports.checkEmailAvailability = (req, res) => {
             return res.status(500).send({ message: `Error checking email availability` });
         }
 
-        // Jeśli nie znaleziono użytkownika o podanym emailu, email jest dostępny
         const available = users.length === 0;
         res.send({ available });
     });
 };
 
-// Dodajemy nową funkcję w authController.js
 exports.checkUsernameAvailability = (req, res) => {
     const { username } = req.params;
 
@@ -146,7 +157,6 @@ exports.checkUsernameAvailability = (req, res) => {
             return res.status(500).send({ message: `Error checking username availability` });
         }
 
-        // Jeśli nie znaleziono użytkownika o podanej nazwie użytkownika, nazwa jest dostępna
         const available = users.length === 0;
         res.send({ available });
     });
@@ -218,7 +228,7 @@ exports.forgotPassword = (req, res) => {
         User.updateResetToken(user.id, resetToken, (err) => {
             if (err) {
                 console.error('Error updating reset token:', err);
-                return res.status(500).send({ message: 'Error generating reset token. Please try again later.' });
+                return res.status(500).send({ message: 'Error during password reset. Please try again later.' });
             }
 
             const resetLink = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
@@ -226,16 +236,16 @@ exports.forgotPassword = (req, res) => {
                 from: process.env.EMAIL,
                 to: email,
                 subject: 'Password Reset',
-                html: `<p>Click the following link to reset your password: <a href="${resetLink}">Reset Password</a></p>`,
+                html: `<p>Please click the following link to reset your password: <a href="${resetLink}">Reset Password</a></p>`,
             };
 
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
-                    console.error('Error sending reset password email:', error);
-                    return res.status(500).send({ message: 'Error sending reset password email. Please try again later.' });
+                    console.error('Error sending password reset email:', error);
+                    return res.status(500).send({ message: 'Error during password reset. Please try again later.' });
                 }
 
-                res.status(200).send({ message: 'Reset password email sent successfully. Please check your email.' });
+                res.status(200).send({ message: 'Password reset email sent successfully. Please check your email.' });
             });
         });
     });
@@ -243,16 +253,16 @@ exports.forgotPassword = (req, res) => {
 
 exports.resetPassword = (req, res) => {
     const { token } = req.params;
-    const { newPassword } = req.body;
+    const { password } = req.body;
 
-    if (!newPassword) {
-        return res.status(400).send({ message: 'New password is required' });
+    if (!password) {
+        return res.status(400).send({ message: 'Password is required' });
     }
 
-    bcrypt.hash(newPassword, 10, (err, hash) => {
+    bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
             console.error('Error hashing password:', err);
-            return res.status(500).send({ message: 'Error hashing password. Please try again later.' });
+            return res.status(500).send({ message: 'Error resetting password. Please try again later.' });
         }
 
         User.resetPassword(token, hash, (err, user) => {
